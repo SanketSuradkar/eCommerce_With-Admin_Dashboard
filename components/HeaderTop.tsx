d3f89b2913b5c742ec51ade6eb6d000619fd4d8a
Sanket S Suradkar <68096112+SanketSuradkar@users.noreply.github.com>
2026-04-22 12:14:39 +0530

    Update email address in HeaderTop component

--- components/HeaderTop.tsx
+++ components/HeaderTop.tsx
@@ -35,7 +35,7 @@ const HeaderTop = () => {
           </li>
           <li className="flex items-center gap-x-2 font-semibold">
             <FaRegEnvelope className="text-white text-xl" />
-            <span>sanket@email.com</span>
+            <span>sanketsuradkar@email.com</span>
           </li>
         </ul>
         <ul className="flex items-center gap-x-5 h-full max-[370px]:text-sm max-[370px]:gap-x-2 font-semibold">
